23f77d048bdb78489d74d98afa7cac62b8a1d61c
William Lees <william@lees.org.uk>
2021-08-01 10:26:09 +0100

    New version 1 Aug 2021

---
+++ copy_airr_seq.bat
@@ -0,0 +1,12 @@
+cp AIRR-seq/Human/IGH/db.sqlite3 ../digby_backend/Human_IGH_db.sqlite3
+cp AIRR-seq/Human/IGH/db_description.txt ../digby_backend/Human_IGH_db_description.txt
+cp AIRR-seq/Human/IGH/samples.zip ../digby_backend/Human_IGH_samples.zip
+cp AIRR-seq/Human/IGK/db.sqlite3 ../digby_backend/Human_IGK_db.sqlite3
+cp AIRR-seq/Human/IGK/db_description.txt ../digby_backend/Human_IGK_db_description.txt
+cp AIRR-seq/Human/IGK/samples.zip ../digby_backend/Human_IGK_samples.zip
+cp AIRR-seq/Human/IGL/db.sqlite3 ../digby_backend/Human_IGL_db.sqlite3
+cp AIRR-seq/Human/IGL/db_description.txt ../digby_backend/Human_IGL_db_description.txt
+cp AIRR-seq/Human/IGL/samples.zip ../digby_backend/Human_IGL_samples.zip
+cp AIRR-seq/Human/TRB/db.sqlite3 ../digby_backend/Human_TRB_db.sqlite3
+cp AIRR-seq/Human/TRB/db_description.txt ../digby_backend/Human_TRB_db_description.txt
+cp AIRR-seq/Human/TRB/samples.zip ../digby_backend/Human_TRB_samples.zip
