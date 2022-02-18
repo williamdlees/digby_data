31ddcb1363a7a214fe67da2cbc9ced68f6b6f9b8
William Lees <william@lees.org.uk>
2022-02-18 14:01:00 +0000

    Fix genomic copy

---
+++ copy_genomic.bat
@@ -0,0 +1,3 @@
+cp "Genomic/Rhesus Macaque/IGH/db.sqlite3" ../digby_backend/Rhesus_Macaque_IGH_genomic_db.sqlite3
+cp "Genomic/Rhesus Macaque/IGH/db_description.txt" ../digby_backend/Rhesus_Macaque_IGH_genomic_db_description.txt
+cp "Genomic/Rhesus Macaque/IGH/samples.zip" ../digby_backend/Rhesus_Macaque_IGH_genomic_samples.zip
