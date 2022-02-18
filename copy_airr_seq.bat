5ac7a639a2e8bd06f3eb0921c389817925f8cce1
William Lees <william@lees.org.uk>
2022-02-18 10:38:22 +0000

    Rhesus fixes

--- copy_airr_seq.bat
+++ copy_airr_seq.bat
@@ -13,4 +13,7 @@ cp AIRR-seq/Human/TRB/samples.zip ../digby_backend/Human_TRB_samples.zip
 
 cp "AIRR-seq/Crab-eating Macaque/IGH/db.sqlite3" ../digby_backend/Crab-eating_Macaque_IGH_db.sqlite3
 cp "AIRR-seq/Crab-eating Macaque/IGH/db_description.txt" ../digby_backend/Crab-eating_Macaque_IGH_db_description.txt
-cp "AIRR-seq/Crab-eating Macaque/IGH/samples.zip" ../digby_backend/Crab-eating_Macaque_IGH_TRB_samples.zip
+cp "AIRR-seq/Crab-eating Macaque/IGH/samples.zip" ../digby_backend/Crab-eating_Macaque_IGH_samples.zip
+cp "AIRR-seq/Rhesus Macaque/IGH/db.sqlite3" ../digby_backend/Rhesus_Macaque_IGH_db.sqlite3
+cp "AIRR-seq/Rhesus Macaque/IGH/db_description.txt" ../digby_backend/Rhesus_Macaque_IGH_db_description.txt
+cp "AIRR-seq/Rhesus Macaque/IGH/samples.zip" ../digby_backend/Rhesus_Macaque_IGH_samples.zip
