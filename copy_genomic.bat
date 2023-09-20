bc123560f2affe8c68c2081fa6bbd55e80f930a9
William Lees <william@lees.org.uk>
2023-09-20 14:24:51 +0100

    Move Genomic to subject/sample structure

--- copy_genomic.bat
+++ copy_genomic.bat
@@ -1,6 +1,3 @@
-cp "Genomic/Rhesus Macaque/IGH/db.sqlite3" ../digby_backend/Rhesus_Macaque_IGH_genomic_db.sqlite3
-cp "Genomic/Rhesus Macaque/IGH/db_description.txt" ../digby_backend/Rhesus_Macaque_IGH_genomic_db_description.txt
-cp "Genomic/Rhesus Macaque/IGH/samples.zip" ../digby_backend/Rhesus_Macaque_IGH_genomic_samples.zip
 cp "Genomic/Human/IGL/db.sqlite3" ../digby_backend/Human_IGL_genomic_db.sqlite3
 cp "Genomic/Human/IGL/db_description.txt" ../digby_backend/Human_IGL_genomic_db_description.txt
 cp "Genomic/Human/IGL/samples.zip" ../digby_backend/Human_IGL_genomic_samples.zip
