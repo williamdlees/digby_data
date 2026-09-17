a47afef128295ba2775e1dced59ccd5f5a2ffbfd
William Lees <william@lees.org.uk>
2026-09-17 17:42:54 +0100

    make archives of QTL subdirectories

--- QTL/Human/IGH/make.bat
+++ QTL/Human/IGH/make.bat
@@ -1,1 +1,7 @@
 python ../../../../digby_backend/make_qtl_db.py Human IGH >make.log
+cd annotation
+zip -r ../annotation.zip *
+cd ..
+cd dbsnp
+zip -r ../dbsnp.zip *
+cd ..
--- QTL/Human/IGL/make.bat
+++ QTL/Human/IGL/make.bat
@@ -1,1 +1,4 @@
 python ../../../../digby_backend/make_qtl_db.py Human IGL >make.log
+cd annotation
+zip -r ../annotation.zip *
+cd ..
