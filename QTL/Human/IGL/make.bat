5a14d7db5ccafc4e4d0872908364e5d5b8abeef6
William Lees <william@lees.org.uk>
2026-09-19 14:35:53 +0100

    rebuild qtl

--- QTL/Human/IGL/make.bat
+++ QTL/Human/IGL/make.bat
@@ -2,3 +2,5 @@ python ../../../../digby_backend/make_qtl_db.py Human IGL >make.log
 cd annotation
 zip -r ../annotation.zip *
 cd ..
+zip db.zip db.sqlite3
+python ../../../../digby_data/python/commit_sample_file.py --file db.zip
